9b5c07660925bb1ab00c93db418d29a44e7cc526
Ghiffary Yuthian <ghiffary.yuthian@gmail.com>
2020-10-12 19:08:23 +0700

    Polybar playerctl prioritize spotify

--- polybar/scripts/get_spotify_status.sh
+++ polybar/scripts/get_spotify_status.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$(playerctl --player=playerctld status)" = "Stopped" ]; then
+if [ "$(playerctl --player=spotify,playerctld status)" = "Stopped" ]; then
     echo "No music is playing"
-elif [ "$(playerctl --player=playerctld status)" = "Paused"  ]; then
+elif [ "$(playerctl --player=spotify,playerctld status)" = "Paused"  ]; then
     polybar-msg -p "$(pgrep -f "polybar main")" hook spotify-play-pause 2 >/dev/null
     playerctl --player=playerctld metadata --format "{{ title }} - {{ artist }}"
 else # Can be configured to output differently when player is paused
--- polybar/scripts/scroll_spotify_status.sh
+++ polybar/scripts/scroll_spotify_status.sh
@@ -3,7 +3,7 @@
 # see man zscroll for documentation of the following parameters
 zscroll -l 30 \
         --delay 0.3 \
-        --match-command "playerctl --player=playerctld status" \
+        --match-command "playerctl --player=spotify,playerctld status" \
         --match-text "Playing" "--scroll 1" \
         --match-text "Paused" "--scroll 0" \
         --update-check true '/home/ghiffaryuthian/.config/bspwm/polybar/scripts/get_spotify_status.sh' &
